38277b63375cb84b3bf47078716deb4cb727baad
fatima <fatimalkhateri@icloud.com>
2024-03-24 01:09:25 +0400

    Initial Commit

---
+++ Digifolio/DigifolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigifolioApp.swift
+//  Digifolio
+//
+//  Created by fatima alkhatri on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DigifolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
